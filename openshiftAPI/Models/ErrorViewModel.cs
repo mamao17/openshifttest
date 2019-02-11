d90f74adca1de8c706d1ccc5e1c00260d115843d
Mo Amao <mo.amao@exxonmobil.com>
2019-02-10 23:54:43 -0600

    initial load

---
+++ openshiftAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace openshiftAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
